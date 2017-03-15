1e2f9ac47f052b4ddc32dfefb0d633c7d2508fba
Tyler Rinker <tyler.rinker@gmail.com>
2017-03-15 00:06:32 -0400

    Update build.R

--- materials/build.R
+++ materials/build.R
@@ -12,7 +12,7 @@ if(pdf){
     path <- find.package(pack)
     system(paste(shQuote(file.path(R.home("bin"), "R")), "CMD", "Rd2pdf", shQuote(path)))
     file.copy(paste0(pack, '.pdf'), file.path(root,"Desktop", paste0(pack, '.pdf')))
-    unlink(paste0(pack, ".pdf"), recursive = TRUE, force = TRUE)
+    while (file.exists(paste0(pack, ".pdf"))) {unlink(paste0(pack, ".pdf"), recursive = TRUE, force = TRUE)}
     empts <- grep("^\\.Rd", dir(all.files = TRUE), value = TRUE)
     unlink(empts, recursive = TRUE, force = TRUE)    
 }
